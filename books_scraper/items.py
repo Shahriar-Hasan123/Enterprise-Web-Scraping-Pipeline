ee7da41fd39a2ca126a8924ec2ed66c4a962a94d
Shahriar Hasan <shahriar@gmail.com>
2026-06-25 16:37:09 +0600

    define BookItem schema with all required fields in items.py

--- books_scraper/items.py
+++ books_scraper/items.py
@@ -1,12 +1,22 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
-class BooksScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class BookItem(scrapy.Item):
+    """
+    Represents a single book scraped from books.toscrape.com.
+
+    Fields:
+        title        (str)  : Book title
+        price        (float): Book price as a number (currency symbol removed)
+        availability (bool) : True if in stock, False otherwise
+        product_url  (str)  : Full URL to the book's detail page
+        image_url    (str)  : Full URL to the book's cover image
+        category     (str)  : Category the book belongs to
+    """
+
+    title = scrapy.Field()
+    price = scrapy.Field()
+    availability = scrapy.Field()
+    product_url = scrapy.Field()
+    image_url = scrapy.Field()
+    category = scrapy.Field()
